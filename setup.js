58b23cb7d9855849d75efcd4bc79f76ff5ccdc12
An <anknguyenanknguyen@gmail.com>
2022-03-30 21:45:13 -0700

    :sparkles: get MST set up

--- setup.js
+++ setup.js
@@ -1,52 +1,152 @@
-let originalPoints = [
-    [-40, 25],
-    [-40, -25],
-    [-40, 0],
-    [-70, 12.5],
-    [-70, -12.5],
+let points = [
+    { x: -40, y: 25, id: "b1" },
+    { x: -40, y: -25, id: "b2" },
+    { x: -40, y: 0, id: "b3" },
+    { x: -70, y: 12.5, id: "b4" },
+    { x: -70, y: -12.5, id: "b5" },
 ];
 
-originalPoints = originalPoints.concat(
-    originalPoints.map(([x, y]) => [x * -1, y])
-);
+let pointsTranslation = [
+    { x: 0, y: 0, id: "b1" },
+    { x: 0, y: 0, id: "b2" },
+    { x: 0, y: 0, id: "b3" },
+    { x: 0, y: 0, id: "b4" },
+    { x: 0, y: 0, id: "b5" },
+];
 
-function dragMoveListener(event) {
-    var target = event.target;
-    const coords = d3.pointer(event);
-    // keep the dragged position in the data-x/data-y attributes
-    console.log(event.dx);
-    var x = (parseFloat(target.getAttribute("data-x")) || 0) + event.dx;
-    var y = (parseFloat(target.getAttribute("data-y")) || 0) + event.dy;
-
-    // translate the element
-    target.style.transform = "translate(" + x + ", " + y + ")";
+function coordTransform(point) {
+    return { ...point, x: point.x + 100, y: 42.5 - point.y };
 }
 
-export function setup() {
-    function coordTransform([x, y]) {
-        return [x + 100, 42.5 - y];
+function mst(points) {
+    // https://favtutor.com/blogs/prims-algorithm-python
+    const N = points.length;
+    // create adjacency matrix
+    let edges = [];
+    let am = [];
+    for (let pointA of points) {
+        let row = [];
+        for (let pointB of points) {
+            row.push(
+                Math.sqrt(
+                    (pointA.x - pointB.x) ** 2 + (pointA.y - pointB.y) ** 2
+                )
+            );
+        }
+        am.push(row);
     }
+    // Prims's algorithm
+    let no_edge = 0;
+    let selected_node = Array(N);
+    selected_node[0] = true;
+    while (no_edge < N - 1) {
+        let minimum = 99999;
+        let a = 0;
+        let b = 0;
+        for (let m = 0; m < N; m += 1) {
+            if (selected_node[m]) {
+                for (let n = 0; n < N; n += 1) {
+                    if (!selected_node[n] && am[m][n]) {
+                        // # not in selected and there is an edge
+                        if (minimum > am[m][n]) {
+                            minimum = am[m][n];
+                            a = m;
+                            b = n;
+                        }
+                    }
+                }
+            }
+        }
+        selected_node[b] = true;
+        no_edge += 1;
+        edges.push([points[a].id, points[b].id]);
+    }
+    return edges;
+}
 
+function lines() {
+    let shifted = _.map(_.zip(points, pointsTranslation), ([p, s]) => ({
+        id: p.id,
+        x: p.x + s.x,
+        y: p.y + s.y,
+    }));
+
+    const edges = mst(shifted);
+
+    d3.select("#mst").selectAll("*").remove();
+    d3.select("#mst")
+        .selectAll("line")
+        .data(edges)
+        .join("line")
+        .attr("x1", (d) => _.find(shifted, ["id", d[0]]).x)
+        .attr("y1", (d) => _.find(shifted, ["id", d[0]]).y)
+        .attr("x2", (d) => _.find(shifted, ["id", d[1]]).x)
+        .attr("y2", (d) => _.find(shifted, ["id", d[1]]).y)
+        .attr("stroke", "black")
+        .attr("stroke-width", "1");
+}
+
+points = points.map(coordTransform);
+
+const root = d3.select("#ice-hockey-svg");
+let rootMatrix;
+
+export function setup() {
+    d3.select("#transformations").append("g").attr("id", "mst");
     let dots = d3
         .select("#transformations")
         .attr("clip-path", "url(#clipBorder)")
         .append("g")
         .attr("id", "dots")
         .selectAll("dot")
-        .data(originalPoints.map(coordTransform))
+        .data(points)
         .join("circle")
-        .attr("cx", (d) => d[0])
-        .attr("cy", (d) => d[1])
+        .attr("cx", (d) => d.x)
+        .attr("cy", (d) => d.y)
+        .attr("data-id", (d) => d.id)
         .attr("r", 3)
         .style("fill", "blue")
         .attr("class", "dot");
 
+    lines(points);
+
     interact(".dot").draggable({
-        inertia: false,
-        autoScroll: false,
         listeners: {
-            // call this function on every dragmove event
-            move: dragMoveListener,
+            start(event) {
+                console.log(event.type, event.target);
+            },
+            move(event) {
+                // regen root Matrix to account for window size changes
+                rootMatrix = root.node().getScreenCTM();
+                let x;
+                let y;
+                pointsTranslation = pointsTranslation.map((point) => {
+                    if (point.id === d3.select(event.target).attr("data-id")) {
+                        point.x += event.dx / rootMatrix.a;
+                        point.y += event.dy / rootMatrix.d;
+                        x = point.x;
+                        y = point.y;
+                    }
+                    return point;
+                });
+                lines();
+                d3.select(event.target).attr(
+                    "transform",
+                    `translate(${x}, ${y})`
+                );
+            },
+            end(event) {
+                console.log();
+            },
         },
     });
+
+    // interact(".dot").draggable({
+    //     inertia: false,
+    //     autoScroll: false,
+    //     listeners: {
+    //         // call this function on every dragmove event
+    //         move: dragMoveListener,
+    //     },
+    // });
 }
