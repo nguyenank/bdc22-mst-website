7b1cfa0e9c6a85f5fb498a46a0340913c96d2d5f
An <anknguyenanknguyen@gmail.com>
2022-04-07 22:04:31 -0700

    add stuff

--- setup.js
+++ setup.js
@@ -1,9 +1,51 @@
 const root = d3.select("#ice-hockey-svg");
 let rootMatrix;
 
+let secondsLastShot = 30;
+
+let bluePoints = [
+    { x: -40, y: 25, id: "O1" },
+    { x: -40, y: -25, id: "O2" },
+    { x: -40, y: 0, id: "O3" },
+    { x: -70, y: 12.5, id: "O4" },
+    { x: -70, y: -12.5, id: "O5" },
+    { x: -55, y: 0, id: "O6" },
+];
+
+let orangePoints = [
+    { x: -20, y: 25, id: "D1" },
+    { x: -20, y: -25, id: "D2" },
+    { x: -20, y: 0, id: "D3" },
+    { x: -50, y: 12.5, id: "D4" },
+    { x: -50, y: -12.5, id: "D5" },
+    { x: -35, y: 0, id: "D6" },
+];
+
 function coordTransform(point) {
     return { ...point, x: point.x + 100, y: 42.5 - point.y };
 }
+
+bluePoints = bluePoints.map(coordTransform);
+orangePoints = orangePoints.map(coordTransform);
+
+let all_points = {
+    blue: {
+        original: _.slice(bluePoints, 0, 5),
+        translation: _.map(_.slice(bluePoints, 0, 5), (p) => ({
+            id: p.id,
+            x: 0,
+            y: 0,
+        })),
+    },
+    orange: {
+        original: _.slice(orangePoints, 0, 5),
+        translation: _.map(_.slice(orangePoints, 0, 5), (p) => ({
+            id: p.id,
+            x: 0,
+            y: 0,
+        })),
+    },
+};
 
 function mst(points) {
     // https://favtutor.com/blogs/prims-algorithm-python
@@ -53,28 +95,44 @@ function mst(points) {
     return [edges, min_total];
 }
 
-function update(points, pointsTranslation, color) {
-    let shifted = _.map(_.zip(points, pointsTranslation), ([p, s]) => ({
+function update(color) {
+    const { original, translation } = all_points[color];
+    let shifted = _.map(_.zip(original, translation), ([p, s]) => ({
         id: p.id,
         x: p.x + s.x,
         y: p.y + s.y,
     }));
 
-    const [edges, min_total] = mst(shifted);
+    let full_shifted = _.flatMap(all_points, (points) => {
+        const { original, translation } = points;
+        return _.map(_.zip(original, translation), ([p, s]) => ({
+            id: p.id,
+            x: p.x + s.x,
+            y: p.y + s.y,
+        }));
+    });
 
+    const [edges, min_total] = mst(shifted);
+    const [full_edges, full_min_total] = mst(full_shifted);
+
+    updateMST(color, min_total, edges, shifted);
+    updateMST("overall", full_min_total, full_edges, full_shifted);
+}
+
+function updateMST(id, min_total, edges, shifted) {
     d3.select("#custom-bar")
-        .select("#" + color)
+        .select("#" + id)
         .select("p")
         .text(min_total.toFixed(2) + "ft");
 
     d3.select("#transformations")
-        .select("#" + color)
+        .select("#" + id)
         .select("#mst")
         .selectAll("*")
         .remove();
 
     d3.select("#transformations")
-        .select("#" + color)
+        .select("#" + id)
         .select("#mst")
         .selectAll("line")
         .data(edges)
@@ -83,16 +141,10 @@ function update(points, pointsTranslation, color) {
         .attr("y1", (d) => _.find(shifted, ["id", d[0]]).y)
         .attr("x2", (d) => _.find(shifted, ["id", d[1]]).x)
         .attr("y2", (d) => _.find(shifted, ["id", d[1]]).y)
-        .attr("class", color);
+        .attr("class", id);
 }
 
 function setUpPointSet(points, color) {
-    let pointsTranslation = _.map(points, (p) => ({
-        id: p.id,
-        x: 0,
-        y: 0,
-    }));
-
     d3.select("#transformations")
         .attr("clip-path", "url(#clipBorder)")
         .select("#" + color)
@@ -129,9 +181,31 @@ function setUpPointSet(points, color) {
                 .attr("dominant-baseline", "middle")
                 .attr("class", "dot-text")
                 .text(d.id);
+            if (d.id === "O1") {
+                d3.select(this)
+                    .append("path")
+                    .attr(
+                        "d",
+                        `M ${d.x + 3} ${d.y} L ${d.x + 4.1} ${d.y + 3.1}`
+                    )
+                    .attr("class", "stick");
+                d3.select(this)
+                    .append("path")
+                    .attr(
+                        "d",
+                        `M ${d.x + 4} ${d.y + 3} L ${d.x + 5.5} ${d.y + 2.85} `
+                    )
+                    .attr("class", "stick-head");
+                d3.select(this)
+                    .append("circle")
+                    .attr("cx", d.x + 4.5)
+                    .attr("cy", d.y + 2)
+                    .attr("r", 0.5)
+                    .attr("class", "pick");
+            }
         });
 
-    update(points, pointsTranslation, color);
+    update(color);
 
     interact("." + color + "-dot").draggable({
         modifiers: [
@@ -142,11 +216,13 @@ function setUpPointSet(points, color) {
         ],
         listeners: {
             move(event) {
+                event.preventDefault();
                 // regen root Matrix to account for window size changes
                 rootMatrix = root.node().getScreenCTM();
                 let x;
                 let y;
-                pointsTranslation = pointsTranslation.map((point) => {
+                let translation = all_points[color].translation;
+                translation = translation.map((point) => {
                     if (point.id === d3.select(event.target).attr("data-id")) {
                         point.x += event.dx / rootMatrix.a;
                         point.y += event.dy / rootMatrix.d;
@@ -155,7 +231,8 @@ function setUpPointSet(points, color) {
                     }
                     return point;
                 });
-                update(points, pointsTranslation, color);
+                all_points[color].translation = translation;
+                update(color);
                 d3.select(event.target).attr(
                     "transform",
                     `translate(${x}, ${y})`
@@ -166,26 +243,26 @@ function setUpPointSet(points, color) {
 }
 
 export function setup() {
-    let bluePoints = [
-        { x: -40, y: 25, id: "A" },
-        { x: -40, y: -25, id: "B" },
-        { x: -40, y: 0, id: "C" },
-        { x: -70, y: 12.5, id: "D" },
-        { x: -70, y: -12.5, id: "E" },
-        { x: -55, y: 0, id: "F" },
-    ];
+    let widget = d3.select("#custom-bar").append("div").attr("id", "overall");
+    widget.append("h3").attr("class", "overall").text("Overall");
 
-    let orangePoints = bluePoints.map((p) => ({ ...p, x: p.x * -1 }));
+    let overallG = d3
+        .select("#transformations")
+        .attr("clip-path", "url(#clipBorder)")
+        .append("g")
+        .attr("id", "overall")
+        .append("g")
+        .attr("id", "mst");
 
-    bluePoints = bluePoints.map(coordTransform);
-    orangePoints = orangePoints.map(coordTransform);
+    widget.append("p").text("0 ft");
 
-    for (const [color, points] of [
-        ["blue", bluePoints],
-        ["orange", orangePoints],
-    ]) {
+    for (const color in all_points) {
+        const points = all_points[color].original;
         let widget = d3.select("#custom-bar").append("div").attr("id", color);
-        widget.append("h3").attr("class", color).text(color);
+        widget
+            .append("h3")
+            .attr("class", color)
+            .text(color === "blue" ? "Offensive" : "Defensive");
 
         let colorG = d3
             .select("#transformations")
@@ -196,7 +273,7 @@ export function setup() {
         widget
             .append("label")
             .attr("for", color + "-select")
-            .text("# Players");
+            .text("# Players:");
 
         let select = widget
             .append("select")
@@ -213,11 +290,53 @@ export function setup() {
 
         select.on("change", function () {
             const value = d3.select(this).property("value");
-            setUpPointSet(_.slice(points, 0, value), color);
+
+            const points = color === "blue" ? bluePoints : orangePoints;
+            all_points[color] = {
+                original: _.slice(points, 0, value),
+                translation: _.map(_.slice(points, 0, value), (p) => ({
+                    id: p.id,
+                    x: 0,
+                    y: 0,
+                })),
+            };
+            setUpPointSet(all_points[color].original, color);
         });
 
         widget.append("p").text("0 ft");
 
         setUpPointSet(_.slice(points, 0, 5), color);
     }
+
+    const sls = d3
+        .select("#custom-bar")
+        .append("div", "second-last-shot-widget");
+    sls.append("label")
+        .attr("id", "seconds-last-shot-label")
+        .attr("for", "seconds-last-shot")
+        .text("Seconds Since Last Shot:");
+    sls.append("input")
+        .attr("id", "seconds-last-shot-range")
+        .attr("name", "seconds-last-shot")
+        .attr("type", "range")
+        .attr("min", 0)
+        .attr("max", 120)
+        .attr("value", secondsLastShot)
+        .on("change", (e) => {
+            secondsLastShot = e.target.value;
+            d3.select("#seconds-last-shot-box").attr("value", secondsLastShot);
+        });
+    sls.append("input")
+        .attr("type", "number")
+        .attr("id", "seconds-last-shot-box")
+        .attr("min", 0)
+        .attr("max", 120)
+        .attr("value", secondsLastShot)
+        .on("change", (e) => {
+            secondsLastShot = e.target.value;
+            d3.select("#seconds-last-shot-range").attr(
+                "value",
+                secondsLastShot
+            );
+        });
 }
